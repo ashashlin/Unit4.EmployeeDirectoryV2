5a9c5dfba517e7fc85bf2cc19b09dac6dbc808e7
Ashley Lin <linashleyy@gmail.com>
2025-05-29 00:41:05 +0800

    Organize /employees routes into a router

--- app.js
+++ app.js
@@ -2,33 +2,36 @@ import express from "express";
 const app = express();
 export default app;
 
-import employees from "#db/employees";
+// import employees from "#db/employees";
+import employeesRouter from "./routes/employees.js";
+
+app.use("/employees", employeesRouter);
 
 app.route("/").get((req, res) => {
   res.send("Hello employees!");
 });
 
-app.route("/employees").get((req, res) => {
-  res.send(employees);
-});
+// app.route("/employees").get((req, res) => {
+//   res.send(employees);
+// });
 
 // Note: this middleware has to come first! Otherwise, Express will treat
 // "random" as the argument to the `id` parameter of /employees/:id.
-app.route("/employees/random").get((req, res) => {
-  const randomIndex = Math.floor(Math.random() * employees.length);
-  res.send(employees[randomIndex]);
-});
+// app.route("/employees/random").get((req, res) => {
+//   const randomIndex = Math.floor(Math.random() * employees.length);
+//   res.send(employees[randomIndex]);
+// });
 
-app.route("/employees/:id").get((req, res) => {
-  const { id } = req.params;
+// app.route("/employees/:id").get((req, res) => {
+//   const { id } = req.params;
 
-  // req.params are always strings, so we need to convert `id` into a number
-  // before we can use it to find the employee
-  const employee = employees.find((e) => e.id === +id);
+//   // req.params are always strings, so we need to convert `id` into a number
+//   // before we can use it to find the employee
+//   const employee = employees.find((e) => e.id === +id);
 
-  if (!employee) {
-    return res.status(404).send("Employee not found");
-  }
+//   if (!employee) {
+//     return res.status(404).send("Employee not found");
+//   }
 
-  res.send(employee);
-});
+//   res.send(employee);
+// });
---
+++ routes/employees.js
@@ -0,0 +1,28 @@
+import express from "express";
+import employees from "#db/employees";
+
+const employeesRouter = express.Router();
+
+employeesRouter.get("/", (req, res) => {
+  res.send(employees);
+});
+
+employeesRouter.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+employeesRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // req.params are always strings, so we need to convert `id` into a number before we can use it to find the employee
+  const employee = employees.find((e) => e.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
+
+export default employeesRouter;
